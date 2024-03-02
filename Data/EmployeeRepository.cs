99e57859dc53d2162aac8bc29be9e994c06bf3f1
UmitAkbas <umit.akbas@hotmail.com.tr>
2024-03-03 02:20:40 +0300

    employeereposiyory added

---
+++ Data/EmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace anguarCrudBackend.Data
+{
+    public class EmployeeRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AddEmployee(Employee employee)
+        {
+            await _appDbContext.Set<Employee>().AddAsync(employee);
+            await _appDbContext.SaveChangesAsync();
+        }
+    }
+}
