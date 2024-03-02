e69ac363ae5b7161cb1e7ce815cd07121a50d5a2
UmitAkbas <umit.akbas@hotmail.com.tr>
2024-03-03 02:01:50 +0300

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace anguarCrudBackend.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Data/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace anguarCrudBackend.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Salary { get; set; }
+
+    }
+}
