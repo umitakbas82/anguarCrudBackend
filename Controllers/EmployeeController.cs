0ea446fabbecbbd82f64902d5843e82877b43054
umitakbas82 <umit.akbas@hotmail.com.tr>
2024-03-04 11:24:56 +0300

    controllers added

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using anguarCrudBackend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace anguarCrudBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+
+        private readonly EmployeeRepository _employeeRepository;
+
+        public EmployeeController(EmployeeRepository employeeRepository) 
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>  AddEmployee([FromBody] Employee model)
+        {
+           await _employeeRepository.AddEmployee(model);
+           return Ok();
+
+
+        }
+
+    }
+}
--- Data/Employee.cs
+++ Data/Employee.cs
@@ -3,9 +3,9 @@
     public class Employee
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
         public int Salary { get; set; }
 
     }
